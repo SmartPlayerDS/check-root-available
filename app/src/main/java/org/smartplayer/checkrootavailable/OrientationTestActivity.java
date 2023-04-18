6506b53e4d731e0c01faad854f243bb4e8175e98
Andrey Nikonov <a.nikonov@smartplayer.org>
2023-04-18 21:19:15 +0300

    Feature. Replace orientation

--- app/src/main/java/org/smartplayer/checkrootavailable/OrientationTestActivity.java
+++ app/src/main/java/org/smartplayer/checkrootavailable/OrientationTestActivity.java
@@ -1,6 +1,7 @@
 package org.smartplayer.checkrootavailable;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -27,44 +28,47 @@ public class OrientationTestActivity extends AppCompatActivity {
     public void onRotate0(View v) {
         logSetRotation.setText("");
         Toast.makeText(this, "Clicked onRotate0", Toast.LENGTH_LONG).show();
+        boolean isUgoos = true;
         executeCommands("settings put system accelerometer_rotation 0");
-        executeCommands("settings put system user_rotation 0");
-        executeCommands("setprop ro.sf.hwrotation 0");
+        executeCommands("setprop media.omx.display_mode 1");
         executeCommands("setprop persist.sys.app.rotation original");
-
+        logSetRotation.append("Will reboot after 10 seconds");
+        new Handler().postDelayed(() -> executeCommands("reboot"), 10000);
     }
 
     public void onRotate90(View v) {
         logSetRotation.setText("");
-        Toast.makeText(this, "Clicked onRotate90", Toast.LENGTH_LONG).show();
         executeCommands("settings put system accelerometer_rotation 0");
-        executeCommands("settings put system user_rotation 90");
-        executeCommands("setprop ro.sf.hwrotation 90");
-        executeCommands("setprop persist.sys.app.rotation original");
-
+        executeCommands("setprop media.omx.display_mode 0");
+        executeCommands("setprop persist.sys.app.rotation force_port");
+        logSetRotation.append("Will reboot after 10 seconds");
+        new Handler().postDelayed(() -> executeCommands("reboot"), 10000);
+        Toast.makeText(this, "Clicked onRotate90", Toast.LENGTH_LONG).show();
     }
 
     public void onRotate180(View v) {
         logSetRotation.setText("");
-        Toast.makeText(this, "Clicked onRotate180", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Clicked onRotate1270", Toast.LENGTH_LONG).show();
         executeCommands("settings put system accelerometer_rotation 0");
-        executeCommands("settings put system user_rotation 180");
-        executeCommands("setprop ro.sf.hwrotation 180");
-        executeCommands("setprop persist.sys.app.rotation original");
+        executeCommands("setprop media.omx.display_mode 0");
+        executeCommands("setprop persist.sys.app.rotation force_land_r");
+        logSetRotation.append("Will reboot after 10 seconds");
+        new Handler().postDelayed(() -> executeCommands("reboot"), 10000);
     }
 
     public void onRotate270(View v) {
         logSetRotation.setText("");
-        Toast.makeText(this, "Clicked onRotate1270", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Clicked onRotate180", Toast.LENGTH_LONG).show();
         executeCommands("settings put system accelerometer_rotation 0");
-        executeCommands("settings put system user_rotation 270");
-        executeCommands("setprop ro.sf.hwrotation 270");
+        executeCommands("setprop media.omx.display_mode 0");
         executeCommands("setprop persist.sys.app.rotation original");
+        logSetRotation.append("Will reboot after 10 seconds");
+        new Handler().postDelayed(() -> executeCommands("reboot"), 10000);
     }
 
     private void executeCommands(String command) {
         try {
-            logSetRotation.append(command + "\n");
+            logSetRotation.append("Command:" + command + "\n");
             Process sh = Runtime.getRuntime().exec("su", null, null);
             OutputStream os = sh.getOutputStream();
             os.write((command).getBytes("ASCII"));
@@ -73,7 +77,10 @@ public class OrientationTestActivity extends AppCompatActivity {
             BufferedReader errorStream = new BufferedReader(new InputStreamReader(sh.getErrorStream(), "UTF-8"));
             String data;
             while ((data = errorStream.readLine()) != null) {
-                Log.d(TAG, "Error sh: " + data);
+                String errorMsg = "Error sh: " + data;
+                Log.e(TAG, errorMsg);
+                logSetRotation.append(errorMsg + "\n");
+
             }
         } catch (IOException e) {
             e.printStackTrace();
